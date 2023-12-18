3352028308b0502a7e36b09904c2ce1ddcc7bb9e
tekna28 <salma.jeghloul@gmail.com>
2023-12-18 13:57:43 -0800

    A function that prints an integer. Using try: / except:

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,6 +2,18 @@
 
 import sys
 def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format().
+
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
+
+    Args:
+    value (int): the integer to print.
+
+    Returns:
+    If a TypeError or ValueError occurs - False.
+    Otherwise - True.
+    """
     try:
         print("{:d}".format(value))
         return (True)
